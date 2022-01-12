e26f93cbf0e4944eb11163d027e3dda432eca513
AlvaroHJ21 <alvarohuaysara@gmail.com>
2022-01-12 09:46:18 -0500

    mas datos de prueba

--- app/src/main/java/com/example/dsm_appcliente/searchModule/searchedProducts/SearchedProductsFragment.kt
+++ app/src/main/java/com/example/dsm_appcliente/searchModule/searchedProducts/SearchedProductsFragment.kt
@@ -82,6 +82,32 @@ class SearchedProductsFragment : Fragment(R.layout.fragment_searched_products),
                     Pair("Volumen neto", "750ml"),
                 )
             ),
+            Product(
+                name = "Red label 5",
+                photoUrl = "https://www.blogdelfotografo.com/wp-content/uploads/2020/12/producto_fondo_negro.webp",
+                nameStore = "Tottus",
+                price = 99.99,
+                description = "Dentro de los licores encontramos al Whisky, una bebida alcohólica a base de malta fermentada de cereales como cebada, trigo, centeno y maíz, que se destila y añeja en barriles de madera tradicionalmente de roble blanco. Este último proceso dura por lo menos tres años para que adquiera el color caramelo que lo caracteriza. El Wisky tiene sus orígenes en Irlanda y Escocia y en la actualidad se disfruta en muchos países a nivel mundial.",
+                specifications = mutableListOf(
+                    Pair("Presentación", "Botella"),
+                    Pair("Composición", "Grano y malta"),
+                    Pair("Proceso de añejamiento", "No declarada"),
+                    Pair("Volumen neto", "750ml"),
+                )
+            ),
+            Product(
+                name = "Red label 6",
+                photoUrl = "https://www.blogdelfotografo.com/wp-content/uploads/2020/12/producto_fondo_negro.webp",
+                nameStore = "Tottus",
+                price = 99.99,
+                description = "Dentro de los licores encontramos al Whisky, una bebida alcohólica a base de malta fermentada de cereales como cebada, trigo, centeno y maíz, que se destila y añeja en barriles de madera tradicionalmente de roble blanco. Este último proceso dura por lo menos tres años para que adquiera el color caramelo que lo caracteriza. El Wisky tiene sus orígenes en Irlanda y Escocia y en la actualidad se disfruta en muchos países a nivel mundial.",
+                specifications = mutableListOf(
+                    Pair("Presentación", "Botella"),
+                    Pair("Composición", "Grano y malta"),
+                    Pair("Proceso de añejamiento", "No declarada"),
+                    Pair("Volumen neto", "750ml"),
+                )
+            ),
         )
     }
 
