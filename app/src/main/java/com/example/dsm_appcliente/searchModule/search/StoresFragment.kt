939a1e7635590ed94ac3ac2732c2256b6fb3fbb1
AlvaroHJ21 <alvarohuaysara@gmail.com>
2022-01-12 19:26:03 -0500

    Interfaz Buscar/Tiendas cercanas v1

--- app/src/main/java/com/example/dsm_appcliente/searchModule/search/StoresFragment.kt
+++ app/src/main/java/com/example/dsm_appcliente/searchModule/search/StoresFragment.kt
@@ -5,10 +5,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.example.dsm_appcliente.R
+import com.example.dsm_appcliente.databinding.FragmentStoresBinding
 
 class StoresFragment : Fragment(R.layout.fragment_stores) {
+
+    private lateinit var mBinding: FragmentStoresBinding
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        mBinding = FragmentStoresBinding.bind(view)
+
+        setupComponents()
+    }
+
+    private fun setupComponents() {
+        Glide.with(mBinding.root)
+            .load("https://paranahaciaelmundo.com/wp-content/uploads/2020/05/captura-de-pantalla-completa-13052020-125022-bmp.jpg")
+            .diskCacheStrategy(DiskCacheStrategy.ALL)
+            .centerCrop()
+            .into(mBinding.imgMaps)
     }
 }
